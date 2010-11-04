da93b7f794a45e3b6c8d14af583c57fa7520ef2c
Takuya Nishimoto <nishimoto@m.ieice.org>
2010-11-04 15:08:51 +0900

    snapshot

---
+++ facerecog.h
@@ -0,0 +1,12 @@
+// opencv-local
+#include <cv.h>
+#include <highgui.h>
+using namespace std;
+using namespace cv;
+
+String cascadeName =
+"/usr/local/share/opencv/haarcascades/haarcascade_frontalface_alt.xml";
+String nestedCascadeName =
+"/usr/local/share/opencv/haarcascades/haarcascade_mcs_eyepair_small.xml";
+
+
---
+++ sprecog.h
@@ -0,0 +1,9 @@
+#ifndef _sprecog_h_
+#define _sprecog_h_
+
+// depends on : julius-local package 
+#include <julius/juliuslib.h>
+
+#endif
+
+
